b0a754a17a4044dda214613790fe8f750a9d71f6
jay <jhennessy78@gmail.com>
2025-03-21 16:44:19 -0700

    update

--- modules/netsec_agent.py
+++ modules/netsec_agent.py
@@ -8,7 +8,7 @@ class Chatbot:
     async def netsec_agent(self, user_input: str) -> str:
         try:
             wd = os.getcwd() 
-            file_path = f"{wd}/modules/skills/netsec_agent.json"
+            file_path = f"{wd}/skills/netsec_agent.json"
             if not os.path.exists(file_path):
                 print(os.path.abspath(file_path))
                 return f"Error: Config file not found at {os.path(file_path)}"
@@ -22,7 +22,7 @@ class Chatbot:
             stream = team.run_stream(task=user_input,cancellation_token=CancellationToken())
             
             async for message in stream:
-                if message.stop_reason:
+                if hasattr(message,'stop_reason'):
                     break
                 print(f"netsec_agent: {message.content}")
             
